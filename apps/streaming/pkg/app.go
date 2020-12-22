58dee7c00650760f4750bed64f06c01f7d323790
Roger King <rogr.king@gmail.com>
2020-12-22 13:14:33 -0500

    chore: cleanup logging

--- apps/streaming/pkg/app.go
+++ apps/streaming/pkg/app.go
@@ -44,7 +44,6 @@ func (s *broadcastServer) CreateStream(req *server.Connection, stream server.Bro
 	}
 
 	log.Printf("User is connected: %s", req.User.DisplayName)
-
 	if currentConns, ok := s.Connections[req.GameId]; ok {
 		s.Connections[req.GameId] = append(currentConns, conn)
 	} else {
@@ -68,11 +67,10 @@ func (s *broadcastServer) StartTimer(req *server.TimerRequest, stream server.Bro
 	if conns, ok := s.Connections[req.GameId]; ok {
 		for _, conn := range conns {
 			wait.Add(1)
-			log.Print(conn.stream)
 			go func(req *server.TimerRequest, stream server.Broadcast_StartTimerServer) {
 				defer wait.Done()
-				log.Printf("Starting counddown: %v", conn.gameID)
-				if conn.active && req.GameId == conn.gameID  {
+				log.Printf("Starting counddown: %v", conn.userID)
+				if req.GameId == conn.gameID  {
 	
 					countdown := 60
 	
@@ -80,23 +78,16 @@ func (s *broadcastServer) StartTimer(req *server.TimerRequest, stream server.Bro
 						countdown--
 						time.Sleep(time.Second * 1)
 						
-						// if !req.IsHost {
-						// 	err := stream.Send(&server.Countdown{Time: int64(countdown)})
-							
-						// 	if err != nil {
-						// 		fmt.Errorf("Failed to send countdown: %v", err)
-						// 	}
-						// }
 						 // TODO: look into a better way than using a global message.
-						 err := conn.stream.Send(&server.Message{Time: int64(countdown)})
-						 if req.IsHost {
+						err := conn.stream.Send(&server.Message{Time: int64(countdown)})
+						if req.IsHost {
 							// ToDO: look into why the connection stream doesnt send to the host too.
 							stream.Send(&server.Countdown{Time: int64(countdown)})
-						 }
-	
-						 if err != nil {
-							 fmt.Errorf("Failed to send countdown: %v", err)
-						 }
+						}
+
+						if err != nil {
+							fmt.Errorf("Failed to send countdown: %v", err)
+						}
 					}
 				}
 			}(req, stream)
@@ -118,12 +109,13 @@ func (s *broadcastServer) NextQuestion(req *server.Question, stream server.Broad
 	done := make(chan int)
 
 	if conns, ok := s.Connections[req.GameId]; ok {
+		log.Print(len(conns))
 		for _, conn := range conns {
 			wait.Add(1)
 			go func(req *server.Question, stream server.Broadcast_NextQuestionServer) {
 				defer wait.Done()
-				if conn.active {
-					log.Print("Sending question to participants")
+				if !conn.isHost && req.GameId == conn.gameID  {
+					log.Printf("Sending question to: ", conn.userID)
 					err := conn.stream.Send(&server.Message{Question: &server.Question{
 						Q: req.Q,
 						Options: req.Options,
